dbb87e2c274ce85cb5dd36d981f37dcf82bd4245
Yury Karalkou <yury.karalkou@gmail.com>
2021-10-22 18:21:14 +0300

    No-ticket: Add types to App.tsx, create-data.tsx, index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import * as React from "react";
-import { Table, IColumn } from "./table";
-import { useMemo, useState } from "react";
-import { createData } from "./create-data";
+import React, { useMemo, useState } from 'react';
+import { Table, IColumn } from './table';
+import { createData } from './create-data';
 
-export default function App() {
-  const columns = useMemo<IColumn[]>(
-    () => [
-      { key: "firstName", title: "First name" },
-      { key: "lastName", title: "Last name" },
-      { key: "age", title: "Age" },
-      { key: "visits", title: "Visits" },
-      { key: "status", title: "Status" },
-      { key: "tags", title: "Tags", render: value => value.join(", ") }
-    ],
-    []
-  );
+const App: React.FC = () => {
+    const columns = useMemo<IColumn[]>(
+        () => [
+            { key: 'firstName', title: 'First name' },
+            { key: 'lastName', title: 'Last name' },
+            { key: 'age', title: 'Age' },
+            { key: 'visits', title: 'Visits' },
+            { key: 'status', title: 'Status' },
+            { key: 'tags', title: 'Tags', render: (value) => value.join(', ') },
+        ],
+        [],
+    );
 
-  const [data] = useState(() => createData(50));
+    const [data] = useState(() => createData(50));
 
-  return (
-    <div>
-      <h1>Test (1 lvl)</h1>
-      <h3>Description of the task in the README.md</h3>
-      <Table columns={columns} data={data} />
-    </div>
-  );
-}
+    return (
+        <div>
+            <h1>Test (1 lvl)</h1>
+            <h3>Description of the task in the README.md</h3>
+            <Table columns={columns} data={data} />
+        </div>
+    );
+};
+
+export default App;
--- src/create-data.ts
+++ src/create-data.ts
@@ -1,33 +1,45 @@
-import * as namor from "namor";
-import * as _ from "lodash";
+import * as namor from 'namor';
+import * as _ from 'lodash';
 
-const generateId = () => {
-  const dateString = Date.now().toString(16);
-  const randomness = Math.random().toString(16).substring(2);
-  return `${dateString}-${randomness}`;
+export interface INewPerson {
+    id: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    visits: number;
+    progress: number;
+    status: 'relationship' | 'complicated' | 'single';
+    tags: Array<string>;
+}
+
+const generateId = (): string => {
+    const dateString = Date.now().toString(16);
+    const randomness = Math.random().toString(16).substring(2);
+    return `${dateString}-${randomness}`;
 };
 
-const newPerson = () => {
-  const statusChance = Math.random();
-  return {
-    id: generateId(),
-    firstName: namor.generate({ words: 1, numbers: 0, saltLength: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0, saltLength: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? "relationship"
-        : statusChance > 0.33
-        ? "complicated"
-        : "single",
-    tags: _.range(_.random(10)).map(() =>
-      namor.generate({ words: 1, numbers: 0, saltLength: 0 })
-    )
-  };
+const getNewPerson = (): INewPerson => {
+    const statusChance = Math.random();
+    const getStatus = () => {
+        if (statusChance > 0.66) return 'relationship';
+        if (statusChance > 0.33) return 'complicated';
+        return 'single';
+    };
+
+    return {
+        id: generateId(),
+        firstName: namor.generate({ words: 1, numbers: 0, saltLength: 0 }),
+        lastName: namor.generate({ words: 1, numbers: 0, saltLength: 0 }),
+        age: Math.floor(Math.random() * 30),
+        visits: Math.floor(Math.random() * 100),
+        progress: Math.floor(Math.random() * 100),
+        status: getStatus(),
+        tags: _.range(_.random(10)).map(() =>
+            namor.generate({ words: 1, numbers: 0, saltLength: 0 }),
+        ),
+    };
 };
 
-export function createData(length: number) {
-  return _.range(length).map(() => newPerson());
+export function createData(length: number): Array<INewPerson> {
+    return _.range(length).map(() => getNewPerson());
 }
