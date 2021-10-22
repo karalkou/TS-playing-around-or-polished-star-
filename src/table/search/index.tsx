f605b361b0fdb240c6c5c41e024a552e1c8c6a9b
Yury Karalkou <yury.karalkou@gmail.com>
2021-10-22 10:45:06 +0300

    No-ticket: Add margin-bottom to the search field

--- src/table/search/index.tsx
+++ src/table/search/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 
+import { StyledInput } from './styled'
+
 interface IProps {
   onSearch: (value: string) => void;
 }
 
 export const Search = React.memo((props: IProps) => {
   return (
-    <input
+    <StyledInput
       type="search"
       onChange={e => props.onSearch(e.target.value)}
       placeholder="Search"
