4973c97571d0eff8033e3351acdf3d45ac53b3cc
Yury Karalkou <yury.karalkou@gmail.com>
2021-10-22 10:46:34 +0300

    No-ticket: Use react-paginate in our Table component

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
+import ReactPaginate from "react-paginate";
 
-import { StyledTable } from "./styled";
 import { Search } from "./search";
+import { StyledTable, StyledPaginateContainer } from "./styled";
 
 export type IValue = any;
 
@@ -18,32 +19,83 @@ interface IProps {
 
 export const Table = React.memo((props: IProps) => {
   const { data, columns } = props;
+  const [pageNumber, setPageNumber] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const setRowsHandler = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    setRowsPerPage(+e.currentTarget.value);
+  }
+
+  const pagesVisited = pageNumber * rowsPerPage;
+
+  const displayTH = () => (
+      columns.map(column => (
+        <th key={column.key}>{column.title}</th>
+      ))
+    )
+  
+  const displayRows = () => data
+    .slice(pagesVisited, pagesVisited + rowsPerPage)
+    .map(item => (
+      <tr key={item.id}>
+        {columns.map(column => (
+          <td key={column.key}>
+            {column.render
+              ? column.render(item[column.key])
+              : item[column.key]}
+          </td>
+        ))}
+      </tr>
+    ));
+
+  const pageCount = Math.ceil(data.length / rowsPerPage)
+
+  const handleChangePage = ({ selected }: any) => {
+    setPageNumber(selected)
+  }
 
   return (
     <div>
       <Search onSearch={value => console.log(value)} />
-      <StyledTable>
-        <thead>
-          <tr>
-            {columns.map(column => (
-              <th key={column.key}>{column.title}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.id}>
-              {columns.map(column => (
-                <td key={column.key}>
-                  {column.render
-                    ? column.render(item[column.key])
-                    : item[column.key]}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </StyledTable>
+      <div style={{marginBottom: '20px'}}>
+        Results per page:&nbsp;
+        <select value={rowsPerPage} onChange={setRowsHandler}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+      </div>
+      {
+        !!rowsPerPage && (
+          <>
+            <StyledTable>
+              <thead>
+                <tr>{displayTH()}</tr>
+              </thead>
+              <tbody>
+                {displayRows()}
+              </tbody>
+          </StyledTable>
+          <StyledPaginateContainer>
+              <ReactPaginate 
+                previousLabel='Prev'
+                nextLabel='Next'
+                pageCount={pageCount}
+                pageRangeDisplayed={pageCount}
+                marginPagesDisplayed={2}
+                onPageChange={handleChangePage}
+                containerClassName='paginationList'
+                pageClassName='paginationListItem'
+                previousLinkClassName='prevBtn'
+                nextLinkClassName='nextBtn'
+                disabledClassName='disabled'
+                activeLinkClassName='active'
+              />
+            </StyledPaginateContainer>
+          </>
+        )
+      }
     </div>
   );
 });
--- src/table/styled.ts
+++ src/table/styled.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const StyledTable = styled.table`
   border-collapse: collapse;
-
   width: 100%;
+  margin-bottom: 20px;
 
   th,
   td {
@@ -15,4 +15,41 @@ export const StyledTable = styled.table`
 export const FlexContainer = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+export const StyledPaginateContainer = styled.div`
+  .paginationList {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    color: #0366d6;
+  }
+  .paginationListItem,
+  .previous,
+  .next {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 20px;
+    min-height: 20px;
+    padding: 0 5px;
+    a {
+      color: black;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+    .active {
+      color: blue;
+      text-decoration: underline;
+    }
+  }
+  .disabled {
+    color: gray;
+    opacity: 0.6;
+  }
 `;
