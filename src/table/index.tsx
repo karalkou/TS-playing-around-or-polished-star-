b24fa87bb44cbfeb0890c19890f919346946aa2b
Yury Karalkou <yury.karalkou@gmail.com>
2022-01-25 14:11:53 +0300

    No-ticket: Comb Table component

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -15,10 +15,10 @@ export interface IColumn {
     render?: (value: Array<string>) => string;
 }
 
-interface IProps {
+export interface IProps {
     columns: IColumn[];
     data: Array<INewPerson>;
-    setData: Dispatch<SetStateAction<INewPerson[]>>;
+    setData?: Dispatch<SetStateAction<INewPerson[]>>;
 }
 
 export interface SelectedItem {
@@ -26,52 +26,45 @@ export interface SelectedItem {
 }
 
 export interface HandleHeadCellClick {
-    (column: IColumn): (e: React.SyntheticEvent) => void;
+    (type: string): (e: React.SyntheticEvent) => void;
 }
 
 export const Table = React.memo((props: IProps) => {
-    const { data, columns, setData } = props;
+    const { data, columns } = props;
     const [pageNumber, setPageNumber] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
 
-    // { age: true, name: true };
-    // or { [key]: true|false }
-    const [dataState, setDataState] = useState<undefined | { [key: string]: boolean }>();
-    console.log('dataState: ', dataState);
-    // const handleSortClick = (key: string): void =>
-    //     setDataState({ [key]: dataState && dataState[key] ? !dataState[key] : true });
+    const [dataState, setDataState] = useState<
+        undefined | { [key: string]: boolean | undefined }
+    >();
 
     const setRowsHandler = (e: React.ChangeEvent<HTMLSelectElement>): void => {
         setRowsPerPage(+e.currentTarget.value);
     };
 
-    const handleHeadCellClick: HandleHeadCellClick = (column) => (_e) => {
-        console.log('column.key: ', column.key);
+    const handleHeadCellClick: HandleHeadCellClick = (type) => (_e) => {
         setDataState({
-            [column.key]: dataState && dataState[column.key] ? !dataState[column.key] : true,
+            [type]: dataState && !dataState[type],
         });
     };
 
     const pagesVisited = pageNumber * rowsPerPage;
     const pageData = data.slice(pagesVisited, pagesVisited + rowsPerPage);
-    const sortableColumnName = dataState && Object.keys(dataState)[0];
-    console.log('sortablebleColsName: ', sortableColumnName);
-    const dataToShow = dataState
-        ? pageData.sort((a, b) => {
-              if (sortableColumnName && a[sortableColumnName] < b[sortableColumnName]) {
-                  return -1;
-              }
-              if (sortableColumnName && a[sortableColumnName] > b[sortableColumnName]) {
-                  return 1;
-              }
-              return 0;
-          })
-        : pageData;
-    console.log(dataToShow);
+    let dataToShow = pageData;
+    if (dataState) {
+        const sortableColumnName = Object.keys(dataState)[0];
+        const direction = sortableColumnName && dataState[sortableColumnName] ? 1 : -1;
+        dataToShow = pageData.sort((a, b) => {
+            if (a[sortableColumnName] === b[sortableColumnName]) {
+                return 0;
+            }
+            return a[sortableColumnName] > b[sortableColumnName] ? direction : direction * -1;
+        });
+    }
 
     const displayTH = (): Array<JSX.Element> =>
         columns.map((column) => (
-            <th key={column.key} onClick={handleHeadCellClick(column)}>
+            <th key={column.key} onClick={handleHeadCellClick(column.key)}>
                 {column.title}
             </th>
         ));
@@ -80,8 +73,6 @@ export const Table = React.memo((props: IProps) => {
         dataToShow.map((item) => (
             <tr key={item.id}>
                 {columns.map((column) => {
-                    // console.log('column: ', column);
-                    // console.log('item[column.key]: ', item[column.key]);
                     return (
                         <td key={column.key}>
                             {column.render ? column.render(item[column.key]) : item[column.key]}
@@ -109,6 +100,21 @@ export const Table = React.memo((props: IProps) => {
                     <option value="20">20</option>
                 </select>
             </div>
+            <button
+                type="button"
+                onClick={() => setDataState(undefined)}
+                style={{
+                    padding: '12px',
+                    marginBottom: '20px',
+                    border: '1px solid black',
+                    fontSize: '14px',
+                    background: 'white',
+                    cursor: 'pointer',
+                }}
+                disabled={!dataState}
+            >
+                Reset sorting
+            </button>
             {!!rowsPerPage && (
                 <>
                     <StyledTable>
@@ -135,9 +141,6 @@ export const Table = React.memo((props: IProps) => {
                     </StyledPaginateContainer>
                 </>
             )}
-            {/* <button type="button" onClick={handleSortClick}>
-                push me
-            </button> */}
         </div>
     );
 });
